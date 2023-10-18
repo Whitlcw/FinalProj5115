8de57978decf2c23b59a0acfd1103a1b1857617f
Whitlcw <58365468+Whitlcw@users.noreply.github.com>
2023-10-18 15:42:32 -0600

    Update DriveSysI2Ctest.py

--- DriveSysI2Ctest.py
+++ DriveSysI2Ctest.py
@@ -6,7 +6,7 @@ bus = smbus.SMBus(1)  # 1 indicates /dev/i2c-1 on Raspberry Pi 4
 arduino_address = 0x08  # Arduino's I2C address
 
 string_to_send = "Hello, Arduino!"
-byte_array = list(map(ord, string_to_send))
+byte_array = [ord(char) for char in string_to_send]
 
 bus.write_i2c_block_data(arduino_address, 0, byte_array)
 
