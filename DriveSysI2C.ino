567b12fb4df5684f514f161537cd0f59bb9c133e
Whitlcw <58365468+Whitlcw@users.noreply.github.com>
2023-10-18 14:40:23 -0600

    Create DriveSysI2C.ino

---
+++ DriveSysI2C.ino
@@ -0,0 +1,28 @@
+/*
+I expect this code to recieve and print a string of characters recieved over i2c
+The next steps, provided this works are to:
+- create a buffer in this code
+- store the string in the buffer as it is recieved
+- parse the string stored in the buffer into usable motor commands
+By: Whit Whittall
+*/
+
+#include <Wire.h>
+
+void setup() {
+  Wire.begin(0x08);  // Set the Arduino as a slave with address 0x08
+  Wire.onReceive(receiveEvent);
+  Serial.begin(9600);
+}
+
+void loop() {
+  delay(100);  // Do something in the loop if needed
+}
+
+void receiveEvent(int byteCount) {
+  while (Wire.available()) {
+    char c = Wire.read();
+    Serial.print(c);
+  }
+}
+
